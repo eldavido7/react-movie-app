5c92b9ec21651989d36a3b7f78e4deab43fdc24e
David <awarri74@gmail.com>
2024-06-10 10:51:35 +0100

    added tmdb attribution

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import "./style.scss";
+import footerImage from './tmdb.svg';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <ContentWrapper>
         <div className="infoText">
-          React Movie Recommendation App
+          React Movie Recommendation App, Powered by TMDB
+        </div>
+        <div className="footerImage">
+          <img src={footerImage} alt="footerImage" />
         </div>
       </ContentWrapper>
     </footer>
